cbe257c4d96c3e56943c7069d72e005e04e78e89
MayuzumiTadahisa <mayutti@silk.plala.or.jp>
2017-05-08 06:57:53 +0900

    RSS読み込み

---
+++ NewsReader/Item.swift
@@ -0,0 +1,12 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by 黛 忠寿 on 2017/05/08.
+//  Copyright © 2017年 黛 忠寿. All rights reserved.
+//
+
+class Item{
+    var title = ""
+    var link = ""
+}
